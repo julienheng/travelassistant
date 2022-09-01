04bacd65660aee1edf122a5700026443ca91a039
Elson Wong <elson.wong@live.com.sg>
2022-09-02 02:11:12 +0800

    front page only mobile view

--- app/models/companion.rb
+++ app/models/companion.rb
@@ -1,4 +1,6 @@
 class Companion < ApplicationRecord
   belongs_to :trip
   validates :name, :age, :type, presence: true
+  has_one_attached :photo
+
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,5 @@ class Trip < ApplicationRecord
   has_many :companions
   validates :start_date, :end_date, :latitude, :longitude, presence: true
   validates :location, :budget, :total_cost, :currency, presence: true
+  has_one_attached :photo
 end
