a6a76adc72bc46c8bb1dd4362a4fbe47ea9510b1
julien heng <hengjchan@gmail.com>
2022-09-06 17:49:10 +0800

    finished backend for companion/new"

--- app/controllers/companions_controller.rb
+++ app/controllers/companions_controller.rb
@@ -12,10 +12,15 @@ class CompanionsController < ApplicationController
     @trip = Trip.find(params[:trip_id])
     @companion = Companion.new(companion_params)
     @companion.trip = @trip
-    if @companion.save
-      redirect_to trip_loading_page_path(@companion.trip)
-    else
-      render :new, status: :unprocessable_entity
+
+    respond_to do |format|
+      if @companion.save
+        format.html { redirect_to trip_loading_page_path(@companion.trip) }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+
+      format.json # views/companions/create.json.jbuilder
     end
   end
 
@@ -25,16 +30,23 @@ class CompanionsController < ApplicationController
     @companion = Companion.find(params[:id])
   end
 
-  def add_field
-    @trip = Trip.find(params[:trip_id])
-    @companion = Companion.new
+  # def add_field
+  #   @trip = Trip.find(params[:trip_id])
+  #   @companion = Companion.new
 
-    respond_to do |format|
-      format.text {
-        render partial: "companions/add_field", locals: { trip: @trip, companion: @companion }, formats: [:html]
-      }
-    end
-  end
+  #   respond_to do |format|
+  #     if @companion.save
+  #       format.html { redirect_to new_trip_companion_path(@companion.trip) }
+  #       format.json
+  #     else
+  #       format.html { render "companions/new", status: :unprocessable_entity }
+  #       format.json
+  #     # format.text {
+  #     #   render partial: "companions/add_field", locals: { trip: @trip, companion: @companion }, formats: [:html]
+  #     # }
+  #     end
+  #   end
+  # end
 
   private
 
