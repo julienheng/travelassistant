2cd97c140acea019a08d44590695401e1aa1dce5
julien heng <hengjchan@gmail.com>
2022-09-07 12:51:57 +0800

    finished companion.new

--- app/models/companion.rb
+++ app/models/companion.rb
@@ -1,7 +1,6 @@
 class Companion < ApplicationRecord
   belongs_to :trip
 
-  validates :name, :age, presence: true
+  validates :name, :age, uniqueness: true, presence: true
   has_one_attached :photo
-
 end
