51c61e43717c89a5e9867098c28fd149ad8a72ac
julien heng <hengjchan@gmail.com>
2022-09-08 18:52:54 +0800

    finished buttons

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -10,7 +10,7 @@ class TripsController < ApplicationController
   def create
     @trip = Trip.new(trip_params)
     @trip.user = current_user
-    raise
+
     if @trip.save
       redirect_to new_trip_companion_path(@trip)
     else
